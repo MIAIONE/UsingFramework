276a61d6925d869576208185e8a3f12f7fa662ec
systemroot <miaione@yeah.net>
2021-06-14 16:57:12 +0800

    20%

--- UsingFramework/OtherProject/UsingFramework.Util/AAL/RequireToken.vb
+++ UsingFramework/OtherProject/UsingFramework.Util/AAL/RequireToken.vb
@@ -2,7 +2,7 @@
     Public Property RequirePermission As RequirePermission
     Public Property AppFiles As String
     Public Property AppName As String
-
+    Public Property GUID As String
 End Class
 Public Class BasePermissionItem
     Public Property Token As Integer
@@ -10,6 +10,20 @@ End Class
 Public Class RequirePermission
     Public Property BasePermission As List(Of BasePermissionItem)
 End Class
+
+Public Class PermissionSetItem
+    Public Property TokenName As String
+    Public Property State As String
+End Class
+
+Public Class ApplicationPermissionSet
+    Public Property RequirePermission As RequirePermission
+    Public Property AppName As String
+    Public Property Md5 As String
+    Public Property PermissionSet As List(Of PermissionSetItem)
+End Class
+
+
 Public Enum Permissions As Integer
     DataProtectionPermission
     EnvironmentPermission

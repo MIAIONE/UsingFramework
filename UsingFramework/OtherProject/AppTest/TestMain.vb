b3239b5fbf04d2f255d303922e3df4506c17130b
systemroot <miaione@yeah.net>
2021-06-20 17:21:15 +0800

    oPENtkC测试

--- UsingFramework/OtherProject/AppTest/TestMain.vb
+++ UsingFramework/OtherProject/AppTest/TestMain.vb
@@ -4,12 +4,9 @@ Public Class TestMain
     Public Function OpenEntryPoint(args As String) As Boolean Implements IPlugin.OpenEntryPoint
         API.TryGetPermission("Test")
         API.WriteLine("Loading Program ")
-        '这里我测试了MMD的第三方NUGET，不过貌似运行不了PMM文件，但是能够运行dll
-        Dim project = New Project().Load(args + "sample.pmm")
-        Dim vv As New Components.View
-        vv.FpsLimit = 30
-        project.View = vv
-        project.Save("0.mp4")
+        Dim DISPLAY As New GameWindow()
+        DISPLAY.Run()
+
         Return True
     End Function
 End Class
